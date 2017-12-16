42119420a0fbfd4a7f4558c97005da48eee24467
Karina Kedrolivanskaya <kedr87@inbox.ru>
2017-12-17 00:10:44 +0300

    1) Основной UI(без поля игры и настроек) 2) Создана бд и Crud запросы к ней 3) Начато выполнение функции авторизации

---
+++ SudokuRep/UserDataRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuRep
+{
+    public class UserDataRepository
+    {
+        private SudokuUsersEntities _dbUsersEntities;
+        public UserDataRepository()
+        {
+            _dbUsersEntities=new SudokuUsersEntities();
+        }
+
+        public void AddOrUpdateGame(Game game)
+        {
+            _dbUsersEntities.Games.Add(game);
+            _dbUsersEntities.SaveChanges();
+        }
+
+        //public List<User> GetLeadersBoard()
+        //{
+        //    _dbUsersEntities.Users.OrderByDescending(
+        //        u=>u.Games.Sum(
+        //            g=>1/g.Duration.GetValueOrDefault())*
+                    
+        //            )
+        //}
+        public void DeleteGame(Game game)
+        {
+            _dbUsersEntities.Games.Remove(game);
+            _dbUsersEntities.SaveChanges();
+        }
+        public void AddOrUpdateUser(User user)
+        {
+            _dbUsersEntities.Users.AddOrUpdate(user);
+            _dbUsersEntities.SaveChanges();
+        }
+        public User GetUserByCredentials(string login,string passHash)
+        {
+            User temp = _dbUsersEntities.Users.First(u => u.login == login);
+            if(temp!=null)
+                if (temp.password == passHash)
+                    return temp;
+            return null;
+        }
+    }
+}
---
+++ SudokuWPF/SudokuModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SudokuRep;
+
+namespace SudokuWPF
+{
+    public class SudokuModel
+    {
+        public SudokuModel()
+        {
+            Users=new ObservableCollection<User>();
+        } 
+        public ObservableCollection<User> Users { get; set; }
+    }
+}
